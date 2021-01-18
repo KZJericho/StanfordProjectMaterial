fdd43aa00028a46f2961f62401eeccbe94144292
KZJericho <KZJericho@gmail.com>
2021-01-17 19:21:36 -0500

    uploaded location tracking, idk why it doens't work

--- PysamV1.py
+++ PysamV1.py
@@ -4,6 +4,45 @@ from Bio.Seq import Seq
 #Bounds --> (Lower, Upper)
 #chr = "chr" + chromosome no.
 #motif --> must be in lowercase
+def CountSTRs(sequence, motif):
+    base = 0
+    CurrLocation = 0
+    BestLocation = 0
+    CurrRepeat = 0
+    BestRepeat = 0
+
+    while base <= (len(sequence)-len(motif)):
+        CurrChunk = sequence[base:base+len(motif)]
+
+        if CurrChunk == motif:
+            CurrRepeat += 1
+            CurrLocation = base
+
+            base += len(motif)
+
+        else:
+            if CurrRepeat > BestRepeat:
+                BestRepeat = CurrRepeat
+                BestLocation = CurrLocation - (len(motif))*((CurrRepeat)-1)
+
+            CurrRepeat = 0
+            base += 1
+
+    if CurrRepeat > BestRepeat:
+        BestRepeat = CurrRepeat
+        BestLocation = CurrLocation - (len(motif))*((CurrRepeat)-1)
+
+
+    return ["length", BestRepeat, "location", BestLocation, (BestLocation+((len(motif)*(BestRepeat))-1))]
+
+'''
+def FindFinalCoordinates(sequence, bounds, motif):
+    result = CountSTRs(sequence, motif)
+    result[3] += bounds[0]
+    result[4] += bounds[1]
+
+    return result
+'''
 def FindRegion(chromosome, Bounds, Motif):
 
     genome = pysam.FastaFile("/home/kevin/Documents/StanfordProjectMaterial/hg19.fa")
@@ -21,31 +60,9 @@ def FindRegion(chromosome, Bounds, Motif):
     if MotifRepeat > ReverseMotifRepeat:
         return Motif, CountSTRs(Sequence, Motif)
     else:
-        return ReverseMotif, CountSTRs(Sequence, ReverseMotif)
+        return ReverseMotif, CountSTRs(Sequence,ReverseMotif)
 
 print(FindRegion("chr1",(57222577,57224042), "aaag" ))
-
-
-def CountSTRs(sequence, motif):
-    base = 0
-    LowerBound = 0
-    HigherBound = 0
-    MotifRepeat = 0
-    while base <= (len(sequence)-len(motif)):
-        CurrChunk = sequence[base:base+len(motif)]
-
-        if CurrChunk == motif:
-            MotifRepeat += 1
-            LowerBound = base - (len(motif)*MotifRepeat)
-            HigherBound == base + len(motif) - 1
-            base += len(motif)
-        else:
-            base +=1
-
-
-    return MotifRepeat, base
-
-
 
 
 
